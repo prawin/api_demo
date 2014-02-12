0e061174a0d771b5641d9faa577ca57c5b3c00cd
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2014-02-12 19:48:57 +0530

    Add access control header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :allow_ajax_request_from_other_domains
+
+  def allow_ajax_request_from_other_domains
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
