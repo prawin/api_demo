debad726100fa90dd1e2cf62743e3233b06ad5a7
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2014-02-14 15:14:28 +0530

    HEaders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
 
   def allow_ajax_request_from_other_domains
     headers['Access-Control-Allow-Origin'] = '*'
-    # headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Request-Methods'] = "POST, GET, PUT, DELETE, OPTIONS"
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
