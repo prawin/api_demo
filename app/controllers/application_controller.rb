c3807ebfaeaddd29d317f3ef10a3beb6a0160e6d
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2014-02-14 13:01:06 +0530

    Access control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   def allow_ajax_request_from_other_domains
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Request-Methods'] = "POST, GET, PUT, DELETE, OPTIONS"
   end
 end
